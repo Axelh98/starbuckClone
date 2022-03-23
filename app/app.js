319cda558455ff031b5229ef74b28ef2d44a3ab8
Axel Herrera <73140275+Axelh98@users.noreply.github.com>
2022-03-23 00:28:49 -0300

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,16 @@
+
+// Variables
+
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;    
+
+}
+
+
+
+
+
